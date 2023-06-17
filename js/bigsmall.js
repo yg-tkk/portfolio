b392ada33f2b1717f20965894e4a4bbc07dce872
yg-tkk <106817315+yg-tkk@users.noreply.github.com>
2023-06-17 23:12:36 +0900

    Create bigsmall.js

---
+++ js/bigsmall.js
@@ -0,0 +1,13 @@
+ $('ul li a').click(function() {
+        var imgSrc = $(this).children().attr('src');
+        $('.bigimg').children().attr('src', imgSrc);
+        $('.modal').fadeIn();
+        $('body,html').css('overflow-y', 'hidden');
+        return false
+      });
+
+$('.close-btn').click(function() {
+        $('.modal').fadeOut();
+        $('body,html').css('overflow-y', 'visible');
+        return false
+      });
